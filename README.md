1977e55dbbc1b1a7b9f1a0af30f4226619bcf966
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-06-20 23:00:57 +0100

    added a little temp test code which I will remove soon

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Orion IoT Web Application Front-end
 The front-end of the web application for the Orion-IoT project
+
+To start this webapp front-end with Docker and Apache web server then go to the "app" folder and run the following 2 commands:
+
+docker build -t orion-iot-webapp-frontend .
+
+docker run -dit --name orion-iot-webapp-frontend-running -p 8081:80 orion-iot-webapp-frontend
--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,40 @@
 'use strict';
 
-$(document).ready(function () {
-    //var $body = $('body');
+$(document).ready(function ()
+{
+    const dataToSend =
+    {
+        field1: 'Dimi'
+    };
+
+
+    fetch('http://localhost:8080/home/test-url',
+    {
+        method: 'POST',
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "include",
+        headers:
+        {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+    })
+    .then(response =>
+    {
+        if(!response.ok)
+        {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        return response.json();
+    })
+    .then(jsonResponse =>
+    {
+        alert(jsonResponse.field2 + "--" + jsonResponse.field1);
+    })
+    .catch(error =>
+    {
+        alert('There has been a problem with your fetch operation:', error);
+    });
 });
