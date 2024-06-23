0c9f7832084687514069e0ae378a6a530115da70
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-06-23 18:38:18 +0100

    first attempt to use WebSocket so that some webpages can automatically receive data from teh server periodically

--- app/js/iot-devices-live.js
+++ app/js/iot-devices-live.js
@@ -1,4 +1,57 @@
-$(document).ready(function ()
-{
-    //orionCommon.fetchComponentData('http://localhost:8080/api/v1/iot-devices/device-details', 'number-of-IoT-devices');
-});
+$(function () {
+    let stompClient;
+
+    const stompConfig =
+    {
+      // Typically, login, passcode and vhost
+      // Adjust these for your broker
+      /*connectHeaders: {
+        login: "guest",
+        passcode: "guest"
+      },*/
+
+      // Broker URL, should start with ws:// or wss:// - adjust for your broker setup
+      brokerURL: "ws://localhost:8080/websocket",
+      reconnectDelay: 500,
+
+      // Subscriptions should be done inside onConnect as those need to reinstated when the broker reconnects
+      onConnect: function(frame)
+      {
+        // The return object has a method called unsubscribe
+        const subscription = stompClient.subscribe('/app/chat/topic/iot-devices-live/summaries', function(message)
+        {
+          const payload = JSON.parse(message.body);
+          displayIncomingMessage(payload.devices);
+        });
+      }
+    };
+
+    stompClient = new StompJs.Client(stompConfig);
+    stompClient.activate();
+
+    setTimeout(function()
+    {
+       publishMessage("hello", "world");
+    }, 1000);
+
+    function publishMessage(from, message)
+    {
+      if(!stompClient.connected)
+      {
+        alert("Broker disconnected, can't send message.");
+        return false;
+      }
+
+      if(message.length > 0)
+      {
+        const payLoad = {from: from, message: message};
+        stompClient.publish({destination: '/app/chat/topic/iot-devices-live/summaries', body: JSON.stringify(payLoad)});
+      }
+      return true;
+    }
+
+    function displayIncomingMessage(from, message)
+    {
+      alert(from + " -------- " + message);
+    }
+  })
