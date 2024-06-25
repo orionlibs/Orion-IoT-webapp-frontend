116335a1f9b43f5c58b343c7d012c9052b172483
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-06-25 14:30:19 +0100

    modal to show the latest N payloads of the selected device

--- app/js/iot-devices-live.js
+++ app/js/iot-devices-live.js
@@ -31,11 +31,11 @@ $(function ()
                 messageBody.devices.forEach(item =>
                 {
                   tableBodyHTML += '<tr>';
-                  tableBodyHTML += '<td>' + item.deviceID + '</td>';
                   tableBodyHTML += '<td>' + item.deviceName + '</td>';
                   tableBodyHTML += '<td>' + item.connectionURL + '</td>';
                   tableBodyHTML += '<td>' + item.payloads + '</td>';
                   tableBodyHTML += '<td><button id="edit-iot-device-' + item.deviceID + '" class="btn btn-info btn--icon-text"><i class="zmdi zmdi-edit"></i> Edit</button></td>';
+                  tableBodyHTML += '<td><button id="open-latest-payloads-of-iot-device-' + item.deviceID + '" class="btn btn-info btn--icon-text"><i class="zmdi zmdi-file"></i> Payloads</button></td>';
                   tableBodyHTML += '</tr>';
                 });
 
@@ -49,6 +49,14 @@ $(function ()
                     $("#input-device-name").val();
                     $("#input-connection-url").val();
                     $('#iot-device-editor-modal').modal('show');
+                });
+
+
+                $('body').on('click', '[id^="open-latest-payloads-of-iot-device-"]', function(e)
+                {
+                    e.preventDefault();
+                    let deviceIDToUse = $(this).prop("id").substring("open-latest-payloads-of-iot-device-".length);
+                    orionCommon.makeGetAJAXCall('http://localhost:8080/wapi/v1/iot-devices/payloads/' + deviceIDToUse + '?latest=true', IoTDevicesLive.processLatestDevicePayloads);
                 });
             }
         });
@@ -64,10 +72,28 @@ $(function ()
 })
 
 
-var IoTDevicesLive =
+let IoTDevicesLive =
 {
     processSuccessfulIoTDeviceDetailsUpdate : function(jsonResponse)
     {
         $("#cancel-update-iot-device-details-button").click();
+    },
+
+
+    processLatestDevicePayloads : function(jsonResponse)
+    {
+        let tableBodyHTML = "";
+
+        jsonResponse.payloads.forEach(item =>
+        {
+            tableBodyHTML += '<tr>';
+            tableBodyHTML += '<td>' + item.topic + '</td>';
+            tableBodyHTML += '<td>' + item.payload + '</td>';
+            tableBodyHTML += '<td>' + item.timestampOfRecord + '</td>';
+            tableBodyHTML += '</tr>';
+        });
+
+        $('#latest-iot-device-payloads').html(tableBodyHTML);
+        $('#iot-device-latest-payloads-modal').modal('show');
     }
 }
--- app/js/iot-devices.js
+++ app/js/iot-devices.js
@@ -26,11 +26,11 @@ $(document).ready(function ()
         data.devices.forEach(item =>
         {
             tableBodyHTML += '<tr>';
-            tableBodyHTML += '<td>' + item.deviceID + '</td>';
             tableBodyHTML += '<td>' + item.deviceName + '</td>';
             tableBodyHTML += '<td>' + item.connectionURL + '</td>';
             tableBodyHTML += '<td>' + item.payloads + '</td>';
             tableBodyHTML += '<td><button id="edit-iot-device-' + item.deviceID + '" class="btn btn-info btn--icon-text"><i class="zmdi zmdi-edit"></i> Edit</button></td>';
+            tableBodyHTML += '<td><button id="open-latest-payloads-of-iot-device-' + item.deviceID + '" class="btn btn-info btn--icon-text"><i class="zmdi zmdi-file"></i> Payloads</button></td>';
             tableBodyHTML += '</tr>';
         });
 
@@ -45,15 +45,41 @@ $(document).ready(function ()
             $("#input-connection-url").val();
             $('#iot-device-editor-modal').modal('show');
         });
+
+
+        $('body').on('click', '[id^="open-latest-payloads-of-iot-device-"]', function(e)
+        {
+            e.preventDefault();
+            let deviceIDToUse = $(this).prop("id").substring("open-latest-payloads-of-iot-device-".length);
+            orionCommon.makeGetAJAXCall('http://localhost:8080/wapi/v1/iot-devices/payloads/' + deviceIDToUse + '?latest=true', IoTDevices.processLatestDevicePayloads);
+        });
     })
     .catch(error =>{document.getElementById("iot-device-details").innerHTML = 'Failed to load data:' + error;});
 });
 
 
-var IoTDevices =
+let IoTDevices =
 {
     processSuccessfulIoTDeviceDetailsUpdate : function(jsonResponse)
     {
         $("#cancel-update-iot-device-details-button").click();
+    },
+
+
+    processLatestDevicePayloads : function(jsonResponse)
+    {
+        let tableBodyHTML = "";
+
+        jsonResponse.payloads.forEach(item =>
+        {
+            tableBodyHTML += '<tr>';
+            tableBodyHTML += '<td>' + item.topic + '</td>';
+            tableBodyHTML += '<td>' + item.payload + '</td>';
+            tableBodyHTML += '<td>' + item.timestampOfRecord + '</td>';
+            tableBodyHTML += '</tr>';
+        });
+
+        $('#latest-iot-device-payloads').html(tableBodyHTML);
+        $('#iot-device-latest-payloads-modal').modal('show');
     }
 }
